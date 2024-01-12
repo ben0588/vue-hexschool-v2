ec969e53557d8f62e01a6dca7ad075cf2f7e33b8
ben0588 <energy95272z@gmail.com>
2024-01-12 20:01:46 +0800

    deploy

---
+++ assets/ErrorView-2eQtN2Q5.js
@@ -0,0 +1,1 @@
+import{_ as o,e as t,c as r,f as n,w as c,b as s,o as a,d as _}from"./index-Ld3t-_nk.js";const l={},d=s("h1",null,"404，路徑不存在，請返回首頁",-1);function i(f,u){const e=t("router-link");return a(),r("div",null,[d,n(e,{to:"/"},{default:c(()=>[_("回首頁")]),_:1})])}const p=o(l,[["render",i]]);export{p as default};
